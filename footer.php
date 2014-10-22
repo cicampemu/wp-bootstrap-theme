cd9c030f0115dfc9f5ba2e71223240b4392e7a84
Etienne Toriello <etienne.toriello@gmail.com>
2014-10-22 16:28:02 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -60,7 +60,7 @@
 		</div>
 		
 		<script src = "http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-		<script src = "<?php bloginfo( 'template_directory' ); ?>/js/bootstrap.js"></script>
+		<script src = "<?php bloginfo( 'template_directory' ); ?>/js/bootstrap.min.js"></script>
 		<?php wp_footer(); ?>
 	</body>
 </html>
